7b85cda6a0a36d94f45bb673563d924289f6b193
chrisgandi <chrisgandi@gmail.com>
2017-01-22 20:38:53 -0600

    inital commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'javascript: The good parts'},
+        {title: 'harry Potter'},
+        {title: 'The dark Tower'},
+        {title: 'Eloquent Ruby'}
+    ]
+}
